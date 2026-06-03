467f2957aad94d0122a6f44851e14924266bb35a
shubhi021 <109888419+shubhi021@users.noreply.github.com>
2026-06-03 18:14:11 +0530

    Implement function to find the middle node of a linked list

---
+++ linked_list/middleNode.js
@@ -0,0 +1,12 @@
+
+var middleNode = function(head) {
+    let fast = head;
+    let slow = head;
+
+    while (fast && fast.next)
+    {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+};

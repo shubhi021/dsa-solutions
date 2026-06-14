4ad11f13af5d37bf06ba63ded21eb60af1880ffd
shubhi021 <109888419+shubhi021@users.noreply.github.com>
2026-06-14 19:38:55 +0530

    Refactor countNodes function for clarity

--- trees/countNodes.js
+++ trees/countNodes.js
@@ -24,3 +24,11 @@ var countNodes = function(root) {
 
     return 1 + countNodes(root.left) + countNodes(root.right);
 };
+
+
+//recursive approach
+var countNodes = function(root) {
+    if (!root) return 0;
+
+    return 1 + countNodes(root.left) + countNodes(root.right);
+};

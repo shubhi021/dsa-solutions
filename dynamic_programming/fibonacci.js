70cd030d0834c12eb5e6132cf7886da5c6e9897a
shubhi021 <109888419+shubhi021@users.noreply.github.com>
2026-06-16 17:54:58 +0530

    Implement Fibonacci function using dynamic programming

---
+++ dynamic_programming/fibonacci.js
@@ -0,0 +1,12 @@
+var fib = function(n) {
+    if(n<=1) return n;
+    let prev1 = 1;
+    let prev2 = 0;
+    for(let i=2;i<=n;i++)
+    {
+        const current = prev1+prev2;
+        prev2= prev1;
+        prev1= current;
+    }
+    return prev1;
+};

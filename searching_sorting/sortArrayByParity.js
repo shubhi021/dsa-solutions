df5ed0bb2cecf349523b84ec69bcb8266a6be749
shubhi021 <109888419+shubhi021@users.noreply.github.com>
2026-07-03 16:49:31 +0530

    Implement function to sort array by parity

---
+++ searching_sorting/sortArrayByParity.js
@@ -0,0 +1,19 @@
+var sortArrayByParity = function (nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        if (nums[left] % 2 === 0) {
+            left++;
+        }
+        else if (nums[right] % 2 !== 0) {
+            right--;
+        }
+        else {
+            [nums[left], nums[right]] = [nums[right], nums[left]];
+            left++;
+            right--;
+        }
+    }
+    return nums;
+};

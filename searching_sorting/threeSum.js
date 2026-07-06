bfbfc407b4d3db96408a263883fea7e6066ad27f
shubhi021 <109888419+shubhi021@users.noreply.github.com>
2026-07-06 18:35:24 +0530

    Implement threeSum function to find triplets

---
+++ searching_sorting/threeSum.js
@@ -0,0 +1,51 @@
+var threeSum = function (nums) {
+
+    nums.sort((a, b) => a - b);
+
+    let result = [];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+
+        // Skip duplicate first elements
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+
+            let sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+
+                result.push([nums[i], nums[left], nums[right]]);
+
+                // Skip duplicate left values
+                while (left < right && nums[left] === nums[left + 1]) {
+                    left++;
+                }
+
+                // Skip duplicate right values
+                while (left < right && nums[right] === nums[right - 1]) {
+                    right--;
+                }
+
+                left++;
+                right--;
+
+            } else if (sum < 0) {
+
+                left++;
+
+            } else {
+
+                right--;
+
+            }
+        }
+    }
+
+    return result;
+};

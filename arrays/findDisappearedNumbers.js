018413b7911a4656bdb6298e382ca344b6d0d7c9
Shubhi Srivastava <109888419+shubhi021@users.noreply.github.com>
2026-09-03 16:34:03 +0530

    Implement function to find disappeared numbers

---
+++ arrays/findDisappearedNumbers.js
@@ -0,0 +1,15 @@
+
+var findDisappearedNumbers = function(nums) {
+const set = new Set(nums)
+const result = [];
+
+for(let i = 1; i<= nums.length; i++)
+{
+    if(! set.has(i) )
+    {
+        result.push(i);
+    }
+}
+return result;
+
+};

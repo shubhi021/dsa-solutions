5dcd35ff05a70e8578b2d1952407773def8e32fd
Shubhi Srivastava <109888419+shubhi021@users.noreply.github.com>
2026-09-04 23:30:04 +0530

    Add checkSorted function for circular array
    Implement a function to check if an array is sorted in non-decreasing order.

---
+++ arrays/checkSorted.js
@@ -0,0 +1,17 @@
+
+var check = function(nums) {
+    let count = 0;
+    const n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > nums[(i + 1) % n]) {
+            count++;
+        }
+
+        if (count > 1) {
+            return false;
+        }
+    }
+
+    return true;
+}

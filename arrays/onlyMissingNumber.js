83d679189319f90151c4d9949387d9eb85d7ce0c
Shubhi Srivastava <109888419+shubhi021@users.noreply.github.com>
2026-09-10 17:30:11 +0530

    Implement function to find missing number in array

---
+++ arrays/onlyMissingNumber.js
@@ -0,0 +1,10 @@
+var missingNumber = function(nums) {
+    let n = nums.length;
+    let expectedSum = n*(n+1)/2;
+    let actualSum = 0;
+    for(let num of nums)
+    {
+        actualSum += num;
+    }
+    return expectedSum - actualSum;
+};

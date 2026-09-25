05440d57898558a488d46854077af3e05c74db20
Shubhi Srivastava <109888419+shubhi021@users.noreply.github.com>
2026-09-25 16:34:03 +0530

    Add function to determine gas station circuit completion

---
+++ arrays/canCompleteCircuitGasStation.js
@@ -0,0 +1,19 @@
+
+var canCompleteCircuit = function (gas, cost) {
+    let totalGas = 0;
+    let currentGas = 0;
+    let start = 0;
+
+    for (let i = 0; i < gas.length; i++) {
+        totalGas += gas[i] - cost[i]
+        currentGas += gas[i] - cost[i]
+        if (currentGas < 0) {
+            start = i + 1;
+            currentGas = 0;
+        }
+    }
+    if (totalGas < 0) {
+        return -1;
+    }
+    return start;
+};
